cd3e66ac39cbf9e81e40b9959902043053ee7d77
ahammednibras8 <ahammednibras@proton.me>
2025-09-29 23:45:55 +0530

    try https://github.com/vercel/next.js/discussions/20650#discussioncomment-4644834

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/blog",
+  assetPrefix: "https://my-blog-sepia-eight.vercel.app",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
