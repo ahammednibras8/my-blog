a075919b7295a3810241e73055f31fac501ac99b
ahammednibras8 <ahammednibras@proton.me>
2025-09-30 13:41:31 +0530

    Added Asset Prefix to next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: "https://my-blog-sepia-eight.vercel.app/",
     reactStrictMode: true,
     output: "standalone",
 };
